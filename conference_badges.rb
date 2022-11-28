1ea65a37e1dc46a6f93f70c631e78c4290c011b2
Aaron David <ajdavid128@gmail.com>
2022-11-27 19:07:13 -0600

    commit tree 8297e40ec632118778ee58a2548e347430f6ae10

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+require 'pry'
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(attendees)
+    attendees.each_with_index do |name, index|
+    room_number = index + 1
+    "Hello, #{name}! You'll be assigned to room #{room_number}!"
+    end
+end
